c29d83feae4fdf3e18775f2e787e86e107da1c11
RealNobody <RealNobody1@cox.net>
2018-09-11 09:31:42 -0700

    The system was getting SitePrism and Capybara aliased functions mixed up resulting in an infinite loop.  This dis-ambiguates the two in the rare cases where it matters.

--- lib/cornucopia/util/multiple_exception_formatter.rb
+++ lib/cornucopia/util/multiple_exception_formatter.rb
@@ -6,7 +6,7 @@ module Cornucopia
       def self.format_backtrace(value)
         return value.to_s unless value.is_a?(Array) && value.all? { |val| val.is_a?(Exception) }
         value_text = value.each_with_object([]) do |error, array|
-          array << "Exception \##{array.length + 1}\n#{error.backtrace.join("\n")}"
+          array << "Exception \##{array.length + 1}\n#{error.message}\n#{error.backtrace.join("\n  ")}"
         end.join("\n\n")
 
         Cornucopia::Util::ReportBuilder.pretty_format(value_text)
--- lib/cornucopia/version.rb
+++ lib/cornucopia/version.rb
@@ -1,3 +1,3 @@
 module Cornucopia
-  VERSION = "0.1.53"
+  VERSION = "0.1.54"
 end
